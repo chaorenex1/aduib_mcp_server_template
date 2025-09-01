cb1e8f5a3ef955653523cb5a79a588524f8a9ea7
chaorenex1 <24537608z@gmail.com>
2025-09-02 00:35:58 +0800

    refactor AsyncUtils to run coroutines in a thread pool with a dedicated event loop

--- utils/async_utils.py
+++ utils/async_utils.py
@@ -1,30 +1,28 @@
 import asyncio
 import threading
+from concurrent import futures
 
+async_thread_pool = futures.ThreadPoolExecutor(thread_name_prefix='async_thread_pool')
 
 class AsyncUtils:
     @classmethod
-    def run_async(cls,coro):
-        try:
-            loop = asyncio.get_running_loop()
-            return loop.run_until_complete(coro)
-        except RuntimeError:
-            # 没有 loop，新建一个
-            return asyncio.run(coro)
-        else:
-            # 有 loop，直接创建任务
-            return loop.create_task(coro)
+    def run_async(cls, coro):
+        """
+            使用线程池在独立事件循环中运行协程任务，
+            主线程阻塞等待结果。
+            """
 
-    @classmethod
-    def get_or_create_event_loop(cls):
-        """Gets or creates an event loop."""
-        try:
-            asyncio.create_task()
-            return asyncio.get_running_loop()
-        except RuntimeError:
+        def run_in_thread(coro):
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            return loop
+            try:
+                return loop.run_until_complete(coro)
+            finally:
+                loop.close()
+
+        # 在线程池中执行协程
+        future = async_thread_pool.submit(run_in_thread, coro)
+        return future.result()
 
 
 
